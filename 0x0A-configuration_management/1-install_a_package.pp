0a9ac093b616d8ab8b5b1c27a9f7bd38c5adbde9
salim aliyu musawa <salemaliyu1992@gmail.co>
2024-01-19 11:53:08 -0800

    flask install

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,12 @@
+# Install a package using Puppet
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install Flask==2.1.0',
+  path        => '/usr/bin',
+  refreshonly => true,
+  require     => Package['python3-pip'],
+}
