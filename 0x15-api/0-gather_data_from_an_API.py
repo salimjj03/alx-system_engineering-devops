67ae612f30f83eeb5020ffe1f3911f0fd618a917
salim aliyu musawa <salemaliyu1992@gmail.co>
2024-02-19 12:54:05 -0800

    api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" This module using this REST API,
+for a given employee ID, returns information
+about his/her TODO list progress. """
+
+import requests
+import sys
+
+query1 = {"id": sys.argv[1]}
+query = {"userId": sys.argv[1]}
+
+with requests.get(
+        "https://jsonplaceholder.typicode.com/users",
+        params=query1
+        ) as result1:
+    pass
+with requests.get(
+        "https://jsonplaceholder.typicode.com/todos",
+        params=query
+        ) as result:
+    total = len(requests.get(
+        "https://jsonplaceholder.typicode.com/todos"
+        ).json())
+    ls = result.json()
+    print("Employee {} is done with tasks({}/{})".format(
+        result1.json()[0].get("name"),
+        len(ls),
+        total
+        ))
+    for i in ls:
+        print("\t", i["title"])
