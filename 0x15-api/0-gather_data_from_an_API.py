f971a382eb563d197765b292c10adc86095e063c
salim aliyu musawa <salemaliyu1992@gmail.co>
2024-02-19 13:04:55 -0800

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,23 +9,26 @@ import sys
 query1 = {"id": sys.argv[1]}
 query = {"userId": sys.argv[1]}
 
-with requests.get(
-        "https://jsonplaceholder.typicode.com/users",
-        params=query1
-        ) as result1:
-    pass
-with requests.get(
-        "https://jsonplaceholder.typicode.com/todos",
-        params=query
-        ) as result:
-    total = len(requests.get(
-        "https://jsonplaceholder.typicode.com/todos"
-        ).json())
-    ls = result.json()
-    print("Employee {} is done with tasks({}/{})".format(
-        result1.json()[0].get("name"),
-        len(ls),
-        total
-        ))
-    for i in ls:
-        print("\t", i["title"])
+
+if __name__ == "__main__":
+
+    with requests.get(
+            "https://jsonplaceholder.typicode.com/users",
+            params=query1
+            ) as result1:
+        pass
+    with requests.get(
+            "https://jsonplaceholder.typicode.com/todos",
+            params=query
+            ) as result:
+        total = len(requests.get(
+            "https://jsonplaceholder.typicode.com/todos"
+            ).json())
+        ls = result.json()
+        print("Employee {} is done with tasks({}/{})".format(
+            result1.json()[0].get("name"),
+            len(ls),
+            total
+            ))
+        for i in ls:
+            print("\t", i["title"])
