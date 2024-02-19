df2ec01493844a6a37bdcdcbd2942ffb9e5c972a
salim aliyu musawa <salemaliyu1992@gmail.co>
2024-02-19 14:38:34 -0800

    api to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" This module using this REST API,
+for a given employee ID, returns information
+about his/her TODO list progress. """
+
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+
+    query1 = {"id": sys.argv[1]}
+    query = {"userId": sys.argv[1]}
+
+    with requests.get(
+            "https://jsonplaceholder.typicode.com/users",
+            params=query1
+            ) as result1:
+        pass
+    with requests.get(
+            "https://jsonplaceholder.typicode.com/todos",
+            params=query
+            ) as result:
+        ls = result.json()
+        name = result1.json()[0].get("name")
+        f_name = "{}.csv".format(sys.argv[1])
+
+        with open(f_name, "w", newline='') as file:
+            writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+            [writer.writerow(
+                [t.get("userId"), name, t.get("completed"), t.get("title")]
+                ) for t in ls]
